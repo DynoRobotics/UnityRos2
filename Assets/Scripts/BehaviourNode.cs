0c9b226c477a2830287798a025aede587fb1c783
Samuel Lindgren <samuellindgren91@gmail.com>
2019-01-19 02:21:58 +0100

    :sparkles: Add simple BehaviourNode helper

---
+++ Assets/Scripts/BehaviourNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourNode : MonoBehaviour
+{
+    public string NodeName = "unity_behavoiur_node";
+
+    rclcs.Context context;
+    protected rclcs.Node node;
+
+    void Awake()
+    {
+        context = new rclcs.Context();
+        rclcs.Rclcs.Init(context);
+        node = new rclcs.Node(NodeName, context);
+    }
+
+    void FixedUpdate()
+    {
+        rclcs.Rclcs.SpinOnce(node, 0.1);
+    }
+}
